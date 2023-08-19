4a87f4312b60a0a8c956d29ff3195b972e0be1ce
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-08-19 16:03:25 +0600

    mobile work start

--- shopping-cart-main/case.js
+++ shopping-cart-main/case.js
@@ -1,26 +1,4 @@
-// function setCaseNumber(update){
-//     const inputValue = document.getElementById('input-value');
-//     const inputValueSring = inputValue.value;
-//     const input = parseInt(inputValueSring);
-   
-//   if{
-//     const incraseValue = input + 1;
-//     inputValue.value = incraseValue;
-//   }
-// }
-// document.getElementById('incrase-case').addEventListener('click',function(){
-//     setCaseNumber(true);
-    
-// })
-// document.getElementById('decrase-case').addEventListener('click',function(){
-//     setCaseNumber();
-//     const decraseValue = input - 1 ;
-//     inputValue.value = decraseValue;
 
-//     const totalCasePrice = decraseValue * 59 ;
-//     const casePrice = document.getElementById('case-price');
-//     casePrice.innerText = totalCasePrice ;
-// })
 
 function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
--- shopping-cart-main/mobile.js
+++ shopping-cart-main/mobile.js
@@ -1,1 +1,29 @@
-console.log('Ratriii');
+function updateMobileNumber(update){
+    const inputFeild = document.getElementById('input-feild');
+    const inputFeildString = inputFeild.value;
+    const inputFeildValue = parseInt(inputFeildString);
+
+    let newMobileNumber;
+    if(update == true){
+        newMobileNumber = inputFeildValue + 1;
+    }
+    else{
+        newMobileNumber = inputFeildValue - 1;
+    }
+    return newMobileNumber;
+}
+
+function mobilePriceUpdate(mobileDisit){
+    const mobilePrice = document.getElementById('mobile-price');
+    const price = mobileDisit * 1219;
+    price.innerText = mobilePrice;
+}
+
+
+document.getElementById('incrase-btn').addEventListener('click',function(){
+   const mobileDisit =  updateMobileNumber(true);
+   mobilePriceUpdate(mobileDisit);
+})
+document.getElementById('dcrase-btn').addEventListener('click',function(){
+    updateMobileNumber(false);
+})
